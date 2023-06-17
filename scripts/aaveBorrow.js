af26fa028a80a8d5402f5548b413c7731c5c0e0b
Daudit <dimo.dev@outlook.com>
2023-06-17 19:03:10 +0300

    feat(getWeth): Implement depositing weth

---
+++ scripts/aaveBorrow.js
@@ -0,0 +1,13 @@
+const { getWeth } = require("../scripts/getWeth")
+
+async function main() {
+    await getWeth()
+    
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.log(error)
+        process.exit(1)
+    })
