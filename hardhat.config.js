0985a5c292a8613f1945b7cb41200c59174891f2
Daudit <dimo.dev@outlook.com>
2023-06-17 19:02:31 +0300

    fix(config): Fix config and add forking url

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,52 +1,53 @@
-require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config();
-require("hardhat-deploy");
+require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config()
+require("hardhat-deploy")
 
-const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
-const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL
 
 module.exports = {
-  solidity: [
-    {version: "0.8.7"},
-    {version: "0.6.6"},
-    {version: "0.4.19"}
-  ],
-  defaultNetwork: "hardhat",
-  networks: {
-    hardhat: {
-      chainId: 31337,
-      blockConfirmations: 1,
+    solidity: {
+        compilers: [{ version: "0.8.7" }, { version: "0.6.6" }, { version: "0.4.19" }],
     },
-    sepolia: {
-      url: SEPOLIA_RPC_URL,
-      accounts: [PRIVATE_KEY],
-      saveDeployments: true,
-      chainId: 11155111,
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            forking: {
+                url: MAINNET_RPC_URL,
+            },
+        },
+        sepolia: {
+            url: SEPOLIA_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            saveDeployments: true,
+            chainId: 11155111,
+        },
     },
-  },
-  gasReporter: {
-    enabled: true,
-    currency: "USD",
-    outputFile: "gas-report.txt",
-    noColors: true,
-    coinmarketcap: COINMARKETCAP_API_KEY,
-  },
-  namedAccounts: {
-    deployer: {
-      default: 0,
+    gasReporter: {
+        enabled: true,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+        coinmarketcap: COINMARKETCAP_API_KEY,
     },
-    player: {
-      default: 1,
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        player: {
+            default: 1,
+        },
     },
-  },
-  mocha: {
-    timeout: 200000, // 200 sec max
-  },
-  etherscan: {
-    apiKey: {
-      sepolia: ETHERSCAN_API_KEY,
+    mocha: {
+        timeout: 200000, // 200 sec max
     },
-  },
-};
+    etherscan: {
+        apiKey: {
+            sepolia: ETHERSCAN_API_KEY,
+        },
+    },
+}
