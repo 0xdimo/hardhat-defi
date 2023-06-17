7ee071416cd81a558d8d028ff8951044043b195d
Daudit <dimo.dev@outlook.com>
2023-06-17 19:24:27 +0300

    chore(config): Add solidity v.0.6.12

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,7 +10,12 @@ const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL
 
 module.exports = {
     solidity: {
-        compilers: [{ version: "0.8.7" }, { version: "0.6.6" }, { version: "0.4.19" }],
+        compilers: [
+            { version: "0.8.7" },
+            { version: "0.6.6" },
+            { version: "0.6.12" },
+            { version: "0.4.19" },
+        ],
     },
     defaultNetwork: "hardhat",
     networks: {
