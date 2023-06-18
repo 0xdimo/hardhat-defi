eecf8f55729ca80cab776ed88cc0b38350fd0a71
Daudit <dimo.dev@outlook.com>
2023-06-18 19:47:44 +0300

    feat(deposit): Deposit collateral

--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -1,5 +1,5 @@
 const { getNamedAccounts } = require("hardhat")
-const { getWeth } = require("../scripts/getWeth")
+const { getWeth, AMOUNT } = require("../scripts/getWeth")
 
 async function main() {
     await getWeth()
@@ -7,6 +7,23 @@ async function main() {
     //Lending pool address Provider (Goerli): ​ 0xb53c1a33016b2dc2ff3653530bff1848a515c8c5
     const lendingPool = await getLendingPool(deployer)
     console.log(`LendingPool address ${lendingPool.address}`)
+
+    //deposit
+    const wethTokenAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
+    //approve
+    await approveErc20(wethTokenAddress, lendingPool.address, AMOUNT, deployer)
+    console.log("Depositing...")
+    await lendingPool.deposit(wethTokenAddress, AMOUNT, deployer, 0)
+    console.log("Deposited!!!")
+}
+
+//You must approve token before deposit
+//We are approving the spender address to withdraw amountToSpend from our account
+async function approveErc20(erc20Address, spenderAddress, amountToSpend, account) {
+    const erc20Token = await ethers.getContractAt("IERC20", erc20Address, account)
+    const tx = await erc20Token.approve(spenderAddress, amountToSpend)
+    await tx.wait(1)
+    console.log("Approved!")
 }
 
 async function getLendingPool(account) {
