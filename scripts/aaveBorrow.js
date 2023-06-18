66c059f874becb4b12e94361c2ae6b9fcca423e7
Daudit <dimo.dev@outlook.com>
2023-06-18 19:32:59 +0300

    feat(getLendingPool): Get lending pool address

--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -1,8 +1,23 @@
+const { getNamedAccounts } = require("hardhat")
 const { getWeth } = require("../scripts/getWeth")
 
 async function main() {
     await getWeth()
-    
+    const { deployer } = await getNamedAccounts()
+    //Lending pool address Provider (Goerli): ​ 0xb53c1a33016b2dc2ff3653530bff1848a515c8c5
+    const lendingPool = await getLendingPool(deployer)
+    console.log(`LendingPool address ${lendingPool.address}`)
+}
+
+async function getLendingPool(account) {
+    const lendingPoolAddressProvider = await ethers.getContractAt(
+        "ILendingPoolAddressesProvider",
+        "0xb53c1a33016b2dc2ff3653530bff1848a515c8c5",
+        account
+    )
+    const lendingPoolAddress = await lendingPoolAddressProvider.getLendingPool()
+    const lendingPool = await ethers.getContractAt("ILendingPool", lendingPoolAddress, account)
+    return lendingPool
 }
 
 main()
